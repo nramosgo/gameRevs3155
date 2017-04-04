9a66f95bf2faad982102d63278101762ee853c9d
David Traccarella <dtraccar@uncc.edu>
2017-04-04 20:51:23 +0000

    add view new, and path working for account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,22 @@
 class AccountsController < ApplicationController
+    def new
+        @account = Account.new
+    end
+    
+    def create
+       
+       @account = Account.new(account_params)
+        
+        if @account.save
+            redirect_to @account
+        else
+            render 'new'
+        end
+    
+    end
 end
+
+private
+    def account_params
+        params.required(:account).permit(:username, :password)
+    end
