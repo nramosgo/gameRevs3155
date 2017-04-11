ec148d34c068ae43142c9124e8300be8ea741727
David Traccarella <dtraccar@uncc.edu>
2017-04-11 20:47:28 +0000

    changed banner picture. added controller methods for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+    def new
+        @account = Account.find(params[:account_id])
+        @review = Review.new
+        
+    end
+    
+    def create
+         @account = Account.find(params[:account_id])
+       
+    end
+
 end
