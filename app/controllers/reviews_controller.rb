52b42b49a14a77790cab79e2701ae97764bb78e1
David Traccarella <dtraccar@uncc.edu>
2017-04-13 20:08:46 +0000

    fixed review form display

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,23 @@
 class ReviewsController < ApplicationController
     def new
-        @account = Account.find(params[:account_id])
         @review = Review.new
         
     end
     
     def create
-         @account = Account.find(params[:account_id])
+        @review = @account.reviews.create(review_params)
        
+        if @review.save
+            redirect_to @review
+        else
+            render 'new'
+        end
     end
+    
 
 end
+
+private
+    def review_params
+        params.required(:review).permit(:title, :mainText, :rating, :date, :image, :tag1, :tag2, :tag3)
+    end
