0352bf83474da9d149a0b07e0525b9a6e4741d99
David Traccarella <dtraccar@uncc.edu>
2017-04-25 21:34:03 +0000

    Added login for existing account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,9 +5,18 @@ class AccountsController < ApplicationController
     
     def create
        
-       @account = Account.new(account_params)
-        
-        if @account.save
+        @account = Account.new(account_params)
+        @userName = @account.userName
+        if Account.exists?(userName: @userName)
+            #get existing account from db and redirect
+            print @userName
+            @account = Account.where(userName: @userName).take
+            @account_id = @account.id
+         
+            redirect_to account_path(@account_id)
+            #render 'new'
+        elsif @account.save
+            #creates new record if not existing and redirects
             redirect_to @account
         else
             render 'new'
@@ -17,10 +26,14 @@ class AccountsController < ApplicationController
     
     def show
         @account = Account.find(params[:id])
+        @account_id=params[:account_id]
+        
+        @articles= Article.where(account_id: @account_id)
+       
     end
 end
 
 private
     def account_params
-        params.required(:account).permit(:userName, :password)
+        params.require(:account).permit(:userName, :password)
     end
--- spec/integration/User_creates_an_acticle_spec.rb
+++ spec/integration/User_creates_an_acticle_spec.rb
@@ -1,42 +1,38 @@
 require 'rails_helper.rb'
 
-feature"User creates a review"do
-    scenario "User successfully navigates to the new review page from the account page" do
+feature "User creates a article"do
+    scenario "User successfully navigates to the new article page from the account page" do
         visit new_account_path
         fill_in "Username", with: "Username Test"
         fill_in "Password", with: "Password Test"
         click_button('Create Account')
-        click_link('Create Review')
-        expect(page).to have_content("NEW REVIEW")
+        click_link('Create Article')
+        expect(page).to have_content("NEW ARTICLE")
         expect(page).to have_field('Title')
         expect(page).to have_field('Maintext')
-        expect(page).to have_field('Rating')
         expect(page).to have_field('Image')
         expect(page).to have_field('Tag1')
         expect(page).to have_field('Tag2')
         expect(page).to have_field('Tag3')
     end
     
-    scenario "User successfully creates a review" do
+    scenario "User successfully creates a article" do
         visit new_account_path
         fill_in "Username", with: "Username Test"
         fill_in "Password", with: "Password Test"
         click_button('Create Account')
-        click_link('Create Review')
+        click_link('Create Article')
         fill_in "Title", with: "Title test"
         fill_in "Maintext", with: "Main test"
-        fill_in "Rating", with: "5"
         fill_in "Image", with: "Image test"
         fill_in "Tag1", with: "Tag1 test"
         fill_in "Tag2", with: "Tag2 test"
         fill_in "Tag3", with: "Tag3 test"
-        click_button('Create Review')
+        click_button('Save Article')
         expect(page).to have_content("Title test")
         expect(page).to have_content("Main test")
-        expect(page).to have_content("5.0/5.0")
         expect(page).to have_content("Tag1 test")
         expect(page).to have_content("Tag2 test")
         expect(page).to have_content("Tag3 test")
     end
 end
-
